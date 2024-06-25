981bcd4cd8035e6a41484c13814fc890536ae0cb
mary free student <masha1207355@gmail.com>
2024-06-25 15:49:52 +0300

    add simple search

---
+++ src/views.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+
+# Функция для определения времени суток
+# def get_greeting():
+#     current_time = datetime.now()
+#     current_hour = current_time.hour
+#     if 6 <= current_hour < 12:
+#         return "Доброе утро"
+#     elif 12 <= current_hour < 18:
+#         return "Добрый день"
+#     elif 18 <= current_hour < 24:
+#         return "Добрый вечер"
+#     else:
+#         return "Доброй ночи"
+#
+#
+# print(get_greeting())
